6f7c10177fdb171c94382f4c9c5dfcb69eb41e7a
Duban Espinosa <duban18aem@gmail.com>
2024-07-26 11:37:34 -0500

    funcion para redireecionamiento a pagina de bienvenida

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const cohete = document.getElementById("cohete");
+
+    cohete.addEventListener("click", function() {
+        // Redirigir a otra página
+        window.location.href = "https://www.ejemplo.com";
+    });
+});
