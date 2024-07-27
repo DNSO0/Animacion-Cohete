da9df96e0b53e3b8904eca10c18639c352bd65d9
Duban Espinosa <duban18aem@gmail.com>
2024-07-27 13:14:45 -0500

    cambio y mejora en el efecto de humo

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const cohete = document.getElementById("cohete");
+document.addEventListener('DOMContentLoaded', () => {
+    const humoContainer = document.querySelector('.humo');
+    const cantidadHumo = 70; // Número de partículas de humo
 
-    cohete.addEventListener("click", function() {
-        // Redirigir a otra página
-        window.location.href = "https://www.ejemplo.com";
-    });
+    function crearHumo() {
+        for (let i = 0; i < cantidadHumo; i++) {
+            const li = document.createElement('li');
+            li.style.width = `${Math.random() * 80 + 150}px`; 
+            li.style.height = `${Math.random() * 40 + 140}px`; // Tamaño entre 60px y 140p
+            li.style.left = `${Math.random() * 54}%`; // Posición horizontal aleatoria (100% para abarcar todo el contenedor)
+            li.style.bottom = `0`; // Alinea la base del humo con la base del contenedor
+            li.style.animationDelay = `${Math.random() * 5}s`; // Retraso de animación aleatorio
+            li.classList.add('humo-item'); // Añadir clase para animación
+            humoContainer.appendChild(li);
+        }
+    }
+
+    crearHumo();
 });
